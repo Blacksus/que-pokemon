c75f1c83d17f7244a98ae180d62dd80ae6cca3b5
Rafael Lopes Aguiar <rafaellopes.aguiar@outlook.com>
2022-03-11 22:25:58 -0300

    Trabalhar com canva no angular é uma canseira... Foda... Atualizado pra versão 13 do angular também

--- src/app/quem/quem.component.ts
+++ src/app/quem/quem.component.ts
@@ -1,169 +1,142 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, ViewChild, AfterViewInit, OnInit } from '@angular/core';
 import { Pokemon } from 'pokenode-ts';
 import { PokeapiService } from '../pokeapi.service';
 import { Observable, OperatorFunction } from 'rxjs';
 import { debounceTime, distinctUntilChanged, map, filter } from 'rxjs/operators';
 import { FormControl } from '@angular/forms';
 import { HttpClient } from '@angular/common/http';
+import { Container, Main } from 'tsparticles';
 
 @Component({
   selector: 'app-quem',
   templateUrl: './quem.component.html',
   styleUrls: ['./quem.component.css']
 })
-export class QuemComponent implements OnInit {
+export class QuemComponent implements OnInit, AfterViewInit {
+  @ViewChild('eleImgPokemon', { static: false }) eleImgPokemon: ElementRef;
+  @ViewChild('canvaPokemon', { static: false }) divCanvaPokemon: ElementRef<HTMLCanvasElement>;
+
   particlesOptions = {
-    fpsLimit: 60,
-    particles: {
-      number: {
-        value: 80,
-        density: {
-          enable: true,
-          value_area: 800
-        }
-      },
+    background: {
       color: {
-        value: '#ff0000',
-        animation: {
-          enable: true,
-          speed: 20,
-          sync: true
-        }
-      },
-      shape: {
-        type: 'circle',
-        stroke: {
-          width: 0
-        },
-        polygon: {
-          nb_sides: 5
-        },
-      },
-      opacity: {
-        value: 0.5,
-        random: false,
-        anim: {
-          enable: false,
-          speed: 3,
-          opacity_min: 0.1,
-          sync: false
-        }
-      },
-      size: {
-        value: 3,
-        random: true,
-        anim: {
-          enable: false,
-          speed: 20,
-          size_min: 0.1,
-          sync: false
-        }
-      },
-      links: {
-        enable: true,
-        distance: 100,
-        color: '#ffffff',
-        opacity: 0.4,
-        width: 1
-      },
-      move: {
-        enable: true,
-        speed: 6,
-        direction: 'none',
-        random: false,
-        straight: false,
-        out_mode: 'out',
-        attract: {
-          enable: false,
-          rotateX: 600,
-          rotateY: 1200
-        }
+        value: '#0d47a1'
       }
     },
+    fpsLimit: 120,
     interactivity: {
-      detect_on: 'canvas',
       events: {
-        onhover: {
-          enable: true,
-          mode: 'repulse'
-        },
-        onclick: {
+        onClick: {
           enable: true,
           mode: 'push'
+        },
+        onHover: {
+          enable: true,
+          mode: 'repulse'
         },
         resize: true
       },
       modes: {
-        grab: {
-          distance: 400,
-          line_linked: {
-            opacity: 1
-          }
-        },
         bubble: {
           distance: 400,
-          size: 40,
           duration: 2,
-          opacity: 0.8
-        },
-        repulse: {
-          distance: 200
+          opacity: 0.8,
+          size: 40
         },
         push: {
-          particles_nb: 4
+          quantity: 4
         },
-        remove: {
-          particles_nb: 2
+        repulse: {
+          distance: 200,
+          duration: 0.4
         }
       }
     },
-    retina_detect: true,
-    background: {
-      color: '#000000',
-      image: '',
-      position: '50% 50%',
-      repeat: 'no-repeat',
-      size: 'cover'
-    }
+    particles: {
+      color: {
+        value: '#ffffff'
+      },
+      links: {
+        color: '#ffffff',
+        distance: 150,
+        enable: true,
+        opacity: 0.5,
+        width: 1
+      },
+      move: {
+        // direction: 'top',
+        enable: true,
+        // outModes: 'bounce',
+        random: false,
+        speed: 6,
+        straight: false
+      },
+      number: {
+        density: {
+          enable: true,
+          area: 800
+        },
+        value: 80
+      },
+      opacity: {
+        value: 0.5
+      },
+      shape: {
+        type: 'circle'
+      },
+      size: {
+        value: { min: 1, max: 5 }
+      }
+    },
+    detectRetina: true
   };
-
+  id = 'tsparticles';
   chute = new FormControl();
   poke: Pokemon;
   imgPokemon: any;
   pokemons: any[];
   filteredOptions: Observable<any[]>;
-  public erros: Pokemon[];
+  public tentativas: Pokemon[];
   public fakeArray = new Array(3);
   public acertou: boolean;
   public acabou: boolean;
   public pokemonSelecionado: any = null;
-
+  private context: CanvasRenderingContext2D | null;
   formatter = (pokemon: Pokemon) => pokemon.name;
 
   constructor(private pokeApi: PokeapiService, private http: HttpClient) { }
 
+  ngAfterViewInit(): void {
+    setTimeout(() => {
+      this.context = (this.divCanvaPokemon.nativeElement as HTMLCanvasElement).getContext('2d');
+      this.eleImgPokemon.nativeElement.remove();
+      this.draw();
+    }, 500);
+  }
+
   ngOnInit(): void {
     this.acabou = false;
     this.acertou = false;
-    this.erros = [];
+    this.tentativas = [];
     this.pokeApi.listarTodosKanto().subscribe(r => this.pokemons = r.results);
 
     this.pokeApi.pegarPokemon(5).subscribe(r => {
       this.poke = r;
-      const pokemonImg = this.poke.sprites.other['official-artwork'].front_default as string;
-
-      this.http.get(pokemonImg, { responseType: 'blob' })
-        .subscribe(blob => {
-          const reader = new FileReader();
-          const binaryString = reader.readAsDataURL(blob);
-          reader.onload = (event: any) => {
-            this.imgPokemon = event.target.result;
-          };
-          reader.onerror = (event: any) => {
-            console.log('File could not be read: ' + event.target.error.code);
-          };
-        });
+      this.imgPokemon = this.poke.sprites.other['official-artwork'].front_default as string;
     });
+  }
+
+  private draw(): void {
+    const image = new Image();
+
+    image.src = this.imgPokemon;
+    this.context.drawImage(image, 0, 0);
+    this.context.scale(-100, -100);
+    this.context.globalAlpha = 0.2;
+    this.context.fillStyle = 'blue';
+    this.context.fillRect(50, 50, 75, 50);
+    this.context.fillStyle = 'green';
+    this.context.fillRect(80, 80, 75, 50);
+    this.context.save();
   }
 
   search: OperatorFunction<string, readonly { id: number, name: string }[]> = (text$: Observable<string>) => text$.pipe(
@@ -174,17 +147,17 @@ export class QuemComponent implements OnInit {
   )
 
   public chutar(): void {
-
     if (this.pokemonSelecionado != null) {
       if (this.pokemonSelecionado.name === this.poke.name) {
+        this.tentativas.push(this.pokemonSelecionado);
         this.acertou = true;
         this.acabou = true;
       }
       else {
         this.fakeArray.pop();
-        this.erros.push(this.pokemonSelecionado);
+        this.tentativas.push(this.pokemonSelecionado);
 
-        if (this.erros.length === 3) {
+        if (this.tentativas.length === 3) {
           this.acabou = true;
         }
       }
